cd9b64b8523d5b370ee6999664457a0c694fd1d4
marynapl <kushnirmarinka@gmail.com>
2020-05-31 13:20:27 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <History />
+        </View>
+      </Provider>
+    )
+  }
 }
